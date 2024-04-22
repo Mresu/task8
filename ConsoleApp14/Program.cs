8a63459a6b4d929b7f787c221a941ea49b71e8ec
Mresu <161798697+Mresu@users.noreply.github.com>
2024-04-22 06:40:51 +0400

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                static t[] list;
+                public CustomList()
+                {
+                    list = Array.Empty<t>();
+                }
+                public void Add(t item)
+                {
+                    Array.Resize(ref list, list.Length + 1);
+                    list[^1] = item;
+                }
+
+                public static t[] Find(t[] list, Predicate<t> method)
+                {
+
+                    t[] findlist = new t[0];
+                    foreach (t t in list)
+                    {
+                        if (method(t))
+                        {
+                            Array.Resize<t>(ref findlist, findlist.Length + 1);
+                            findlist[^1] = t;
+                            Console.WriteLine(t);
+                            break;
+                        }
+                    }
+                    return findlist;
+
+                }
+
+                public static t[] FindAll(t[] list, Predicate<t> method)
+                {
+                    t[] findlist = new t[0];
+
+                    foreach (t item in list)
+                    {
+                        if (method(item))
+                        {
+                            Array.Resize<t>(ref findlist, findlist.Length + 1);
+                            findlist[^1] = item;
+                            Console.WriteLine(item);
+                        }
+
+                    }
+                    return findlist;
+
+
+                }
+            }
+    }
+}
